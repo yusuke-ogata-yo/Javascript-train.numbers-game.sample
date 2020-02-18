527485c00703e53c8f865b8e85ac928c30b4b016
yusuke ogata <yusuke.ogata.0618.dev@gmail.com>
2020-02-18 16:11:40 +0900

    [update] lesson#08 コメントの追加

--- js/main.js
+++ js/main.js
@@ -2,25 +2,42 @@
 
 {
   class Panel {
+    /**
+     * コンストラクタ
+     */
     constructor() {
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
 
+    /**
+     * el要素を返すゲッター
+     */
     getEl() {
       return this.el
     }
   }
 
   class Board {
+    /**
+     * コンストラクタ。数値パネルの生成、
+     */
     constructor() {
+      /**
+       * 数値パネルの配列
+       * @type Panel[]
+       */
       this.panels = [];
+      // 数値パネルの生成
       for (let i = 0; i < 4; i++) {
         this.panels.push(new Panel());
       }
       this.setup();
     }
 
+    /**
+     * 数値パネルを#board要素に追加する
+     */
     setup() {
       const board = document.getElementById('board');
       this.panels.forEach(panel => {
@@ -29,5 +46,9 @@
     }
   }
 
+  /**
+   * board要素
+   * @type Board
+   */
   const board = new Board();
 }
